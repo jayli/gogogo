0c8b7665aaca1127209e46d1f88584993adb0aec
lijing00333 <lijing00333@163.com>
2019-01-03 12:45:20 +0800

    bugfix

--- examples/witch_is_faster/cal_goroutine.go
+++ examples/witch_is_faster/cal_goroutine.go
@@ -10,8 +10,11 @@ func run(i, n int64, ch chan int64) {
 		count = count + i
 	}
 	ch <- count
-	asdf := 1
-	asdf += 1
+	log("CPU runing")
+}
+
+func log(s string) {
+	fmt.Printf(">>> %s\n", s)
 }
 
 func main() {
@@ -22,13 +25,15 @@ func main() {
 	chs := make([]chan int64, NCPU)
 	for i := int64(0); i < int64(NCPU); i++ {
 		chs[i] = make(chan int64)
-		n := int64(9000000000) / int64(NCPU)
+		n := int64(900000000) / int64(NCPU)
 		go run(i*n, (i+1)*n, chs[i])
+		log("main new chan")
 	}
 
 	count := int64(0)
 	for i := 0; i < NCPU; i++ {
 		t := <-chs[i] // 这里会等待 chs[i] 被赋值了才执行
+		log("get chans results")
 		count = count + t
 	}
 	t2 := time.Now()
