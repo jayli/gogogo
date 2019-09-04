1aa01f5e6cae63396d2d9353b7eca52f8dffa292
lijing00333 <lijing00333@163.com>
2019-09-04 18:09:15 +0800

    python demo added

---
+++ p/modules_example/index.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+sys.path.append(os.path.abspath("./module1")) 
+
+import ma
+
+
+#--------------------
+
+def test_case_demo():
+    """docstring for test_case_demo"""
+
+    print(__name__)
+    ma.say()
+    print(sys.path)
+    print(dir(ma))
+    print("----------------------")
+    
+
+
+def sum_demo():
+    """docstring for sum_demo"""
+    return
+    a = input('a:')
+    b = input('b:')
+    sum = int(a) + int(b)
+    print('{0} + {1} = {2}'. format(a,b, sum))
+
+if __name__ == "__main__":
+    test_case_demo()
+    sum_demo()
+    
---
+++ p/modules_example/module1/ma.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def say():
+    """docstring for say"""
+    print("saying")
+
+def read():
+    """docstring for read"""
+    print("read")
+    
