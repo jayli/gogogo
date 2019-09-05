6a4e50f098c1e21ac2ff54f88f72eebac81ea250
lijing00333 <lijing00333@163.com>
2019-09-05 21:29:50 +0800

    bugfix

--- python/inherit/all.py
+++ python/inherit/all.py
@@ -15,8 +15,6 @@ def init():
     b = B()
     print(dir(type(F())))
 
-    object_tree(F())
-
     print("----------------")
 
     o = CreateObject({
@@ -34,20 +32,66 @@ def init():
 
     print(F.__bases__)
 
-# def parse_classes_to_object(klass):
-#     klass_obj = {'name':klass.__name__, 'super_classes':{}}
-#     for super_classes in klass.__bases__:
-#         klass_obj = 
+    print("----------------")
+
+    full_output = object_tree(F())
+    print(full_output)
+    print("---------------->>")
+    print(modify_node(0, full_output))
 
 
-def class_tree(cls, indent):
-    print(" " * indent , "└─", cls.__name__)
+def class_tree(cls, level , full_output):
+    line_output = []
+    
+    index = 1
+    if level <= 1:
+        line_output = [" ",cls.__name__]
+    else:
+        while index < level:
+            line_output.append(" ")
+            index += 1
+        line_output.extend(["└", cls.__name__])
+    
+    full_output.append(line_output)
+    # print(" " * level, "└─", cls.__name__)
     for supcls in cls.__bases__:
-        class_tree(supcls, indent + 3)
+        class_tree(supcls, level + 1, full_output)
+
+    return full_output
 
 def object_tree(obj):
     print("Tree of", obj)
-    class_tree(obj.__class__, 3)
+    return class_tree(obj.__class__, 1, [])
 
+# 0,1,2,3...
+def modify_node(line_number, full_output):
+    node_level = len(full_output[line_number])
+    print(full_output[line_number])
 
+    if line_number == len(full_output) - 1:
+        return full_output
 
+    myroot = 0
+    index = 0
+    while index < line_number:
+        # 寻找根节点
+        tmp = full_output[index]
+        if len(tmp) == node_level - 1: # 找到根节点
+            myroot = index
+            break
+        index += 1
+
+    # TODO 这里有问题
+    tdex = myroot + 1
+    while tdex < line_number:
+        tmp = full_output[tdex]
+        if len(tmp) == node_level:
+            tmp[node_level - 2] = "├"
+        else:
+            tmp[node_level - 2] = "|"
+        print(tmp)
+        tdex += 1
+
+    return modify_node(line_number + 1, full_output)
+
+    
