0a7c6e43a005ad53986f670fa7bb878ea63c9db9
lijing00333 <lijing00333@163.com>
2019-09-06 11:11:32 +0800

    除了寻找myroot不是倒叙的，其他应该都ok

--- python/inherit/all.py
+++ python/inherit/all.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from dotmap import DotMap as CreateObject
+import re
 
 class A:       pass
 class B(A):    pass
@@ -35,10 +36,22 @@ def init():
     print("----------------")
 
     full_output = object_tree(F())
-    print(full_output)
     print("---------------->>")
-    print(modify_node(0, full_output))
 
+    full_output = trim_tree_list(modify_node(0,full_output))
+    show_the_tree(full_output)
+
+
+def trim_tree_list(full_output):
+    for arr in full_output:
+        if re.match(r"^\s*$",arr[0]):
+            arr.pop(0)
+    return full_output
+
+def show_the_tree(full_output):
+    for item in full_output:
+        print("".join(item))
+    return full_output
 
 def class_tree(cls, level , full_output):
     line_output = []
@@ -63,33 +76,41 @@ def object_tree(obj):
     print("Tree of", obj)
     return class_tree(obj.__class__, 1, [])
 
-# 0,1,2,3...
+# line_number: 当前游标所在的行索引,0,1,2,3,4...
+# full_output: 当前可视结构的全量数组
+# return: 返回修正之后的全量数组
 def modify_node(line_number, full_output):
-    node_level = len(full_output[line_number])
-    print(full_output[line_number])
+    # 当前数组长度, 1,2,3,4...
+    current_length = len(full_output[line_number])
 
+    # 如果遍历结束，直接返回全量数组，结束递归
     if line_number == len(full_output) - 1:
         return full_output
 
+    # 找到当前行所属的根节点位置（0,1,2,3...）
     myroot = 0
     index = 0
     while index < line_number:
         # 寻找根节点
         tmp = full_output[index]
-        if len(tmp) == node_level - 1: # 找到根节点
+        if len(tmp) == current_length - 1: # 找到根节点
             myroot = index
             break
         index += 1
+    # print("root_node", myroot)
 
+    # 找到所属根节点后，修改连接线样式
     # TODO 这里有问题
     tdex = myroot + 1
     while tdex < line_number:
         tmp = full_output[tdex]
-        if len(tmp) == node_level:
-            tmp[node_level - 2] = "├"
-        else:
-            tmp[node_level - 2] = "|"
-        print(tmp)
+        if len(tmp) == current_length:
+            if len(full_output[tdex + 1]) > len(tmp):
+                tmp[current_length - 2] = "├"
+            else: pass
+        elif len(tmp) > current_length:
+            tmp[current_length - 2] = "│"
+        else: pass
         tdex += 1
 
     return modify_node(line_number + 1, full_output)
