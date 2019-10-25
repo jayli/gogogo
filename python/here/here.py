cd8e0cebae112055545d9a46ffbf55bd56bd2663
lijing00333 <lijing00333@163.com>
2019-10-25 19:02:39 +0800

    bugfix

--- python/here/here.py
+++ python/here/here.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import http.server
+import os
 from urllib.parse import urlparse
 
-def runServer(server_class=http.server.HTTPServer, handler_class=http.server.SimpleHTTPRequestHandler):
-    server_addr = ("", 3000)
-    httpd = server_class(server_addr, handler_class)
+__version__ = "0.1"
+
+class HereProxyHandler(http.server.BaseHTTPRequestHandler):
+
+    server_version = "HereProxyHandler/" + __version__
+
+    def __init__(self, *args, directory=None, **kwargs):
+        if directory is None:
+            directory = os.getcwd()
+        self.directory = directory
+        super().__init__(*args, **kwargs)
+
+    def do_GET(self):
+        __import__('pdb').set_trace()
+        filepath = self.translate_path(self.path)
+        # TODO Here
+        pass
+
+    def do_HEAD(self):
+        pass
+
+    def translate_path(self, path=""):
+        # http://localhost:3000/path/to/file
+        # =>
+        # pwd/path/to/file
+        filepath = self.directory + path
+        return filepath
+
+def run_server(handler_class=HereProxyHandler):
+    httpd = http.server.HTTPServer(("", 3000), handler_class)
     httpd.serve_forever()
 
 def main():
-    runServer()
+    run_server()
 
 if __name__ == '__main__':
     main()
--- python/iter/main.py
+++ python/iter/main.py
@@ -32,13 +32,13 @@ def test1():
     """docstring for test1"""
     info = [1,2,3,4,5,6,7,8]
     print(list(range(1,11)))
-    
+
 def fibonacci(n):
     """docstring for fibonacci"""
 
     a , b , conter = 0 , 1  ,0
     fib_list = [0, 1]
-    
+
     while conter <= n:
         if conter < 2:
             conter += 1
@@ -46,11 +46,11 @@ def fibonacci(n):
         else: pass
         fib_list.append(fib_list[conter - 2] + fib_list[conter - 1])
         conter += 1
-    
+
     print(fib_list)
 
     return None
-    
+
 def test_turtle():
     def drawStar(x, y):
         pu()
@@ -68,4 +68,4 @@ def test_turtle():
     done()
 
     return None
-    
+
