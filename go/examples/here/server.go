3b85d5336ffd7f6e2b816cd4906585c31973a794
lijing00333 <lijing00333@163.com>
2019-11-06 16:18:46 +0800

    bugfix

--- go/examples/here/server.go
+++ go/examples/here/server.go
@@ -6,9 +6,13 @@ import (
 )
 
 func main() {
+	a := 1
+	bb := 2
+	ccc := 3
+	dddd := a + bb + ccc
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
+		return c.String(http.StatusOK, "Hello World!"+string(dddd))
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
