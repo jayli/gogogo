2a6fef6a69ed360df767de5f67b8565bf35ef6ea
lijing00333 <lijing00333@163.com>
2019-11-27 18:16:51 +0800

    bugfix

--- python/dump/example.py
+++ python/dump/example.py
@@ -2,8 +2,9 @@
 # debugger_entry = ../index.py
 # -*- coding: utf-8 -*-
 
-from dump.dump import dump
+#from dump.dump import dump
 import time, sys
+import json
 
 class A:       pass
 class B(A):    pass
@@ -26,10 +27,11 @@ def init():
     dump({'a','b','c','d'})
     dump(A)
     dump(F)
+    print('--')
     dump(F())
     dump(F.__call__)
     print('--')
-    see(1)
+    #see(1)
     dump(abs)
     dump(lambda x:x)
     dump((x for x in range(10)))
@@ -37,16 +39,50 @@ def init():
     dump(time)
     dump(sys.implementation)
     dump(type.__dict__)
+    dump([].append)
+    dump(object.__init__)
+    dump(object().__str__)
+    dump(str.join)
+    dump(dict.__dict__['fromkeys'])
+    dump(sys)
+    dump(type(dump).__code__)
+    dump(type(dump).__globals__)
+
+    dump(_cell_factory())
+    async def _c(): pass
+    _c = _c()
+    dump(_c)
+    _c.close()
+
+    async def _ag():
+        yield
+    _ag = _ag()
+    dump(_ag)
+
     print('end')
     return None
 
+
+def _cell_factory():
+    a = 1
+    def f():
+        nonlocal a
+    return f.__closure__[0]
+
+def dump(p):
+    print(type(p))
+
+
 def see(p):
     li = []
+    li_dump = {}
     for a in dir(p):
         li.append({
             'name':a,
             'value':p.__getattribute__(a),
         })
-    for b in li:
-        print(b['name'], " => ", b['value'])
+        li_dump[a] = str(p.__getattribute__(a))
+    # for b in li:
+    #     print(b['name'], " => ", b['value'])
+    print(json.dumps(li_dump, sort_keys=True, indent=2))
     return li
