89b00998169e56b322e6f0011415a34a162a0d84
lijing00333 <lijing00333@163.com>
2019-12-12 17:58:04 +0800

    bugfix

--- python/test/a.py
+++ python/test/a.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def print_log():
-    print("ok")
+def resolve(num):
+    i = 2
+    while i <= num:
+        if num % i == 0:
+            print(i)
+            resolve(num / i)
+            break
+        i = i + 1
+        continue
 
-def foo(a=""):
-    __import__('pdb').set_trace()
-    pass
-
-def main():
-    print_log()
-    foo()
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    resolve(22)
