63e56295e3055d12a8a36dbc774582f7bd783c9b
jayli <lijing00333@163.com>
2021-10-31 15:10:10 +0800

    bugfix

--- node/crawler/index.js
+++ node/crawler/index.js
@@ -1,6 +1,25 @@
 // curl
 
-const https = require('https')
+const https = require('https');
+
+(async () => {
+
+  https.get("https://jsonbin.org/me/urls", {
+    method: "GET",
+    headers: {
+      "authorization": 'token bb2fbb2f-1b6f-4b2f-bb66-6ba7e9f412e4'
+    }
+  }, (res) => {
+    console.log(res.statusCode)
+    res.setEncoding('utf8')
+    let rawData = '';
+    res.on('data', chunk => { rawData += chunk; })
+    res.on('end', () => {
+      console.log('--- response end ---')
+      console.log(rawData)
+    })
+  })
+
+})()
 
 
-console.log(https)
